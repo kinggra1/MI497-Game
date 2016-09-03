a2bbe6551bad0e6eafbbc38da77e32416c695951
kinggra1 <kinggra1@msu.edu>
2016-09-03 17:48:59 -0400

    Camera does fixed offset and rotation

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class CameraController : MonoBehaviour {
 
+    public CharacterController playerController;
+    private Transform playerTransform;
+
+    private Vector3 targetPos { get; set; }
+    private Quaternion targetRot;
+
 	// Use this for initialization
 	void Start () {
-	
+        playerTransform = playerController.transform;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        targetPos = playerTransform.position + new Vector3(0f, 3f, -10f);
+        targetRot = Quaternion.Euler(10f, 0f, 0f);
+
+        transform.position = targetPos;
+        transform.rotation = targetRot;
 	}
+
+
 }
